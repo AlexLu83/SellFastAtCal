2c4a156110ed62b116643d9646844eba393484ca
Jiajun Lu <jlu82@berkeley.edu>
2020-05-22 23:06:46 -0700

    x

--- SellFast/app/models/order.rb
+++
@@ -1,8 +0,0 @@
-class Order < ApplicationRecord
-    validates :seller_id, :buyer_id, :status, :amount, :item_id, presence: true
-
-	def self.all_status
-		%w(:PENDING_ACTION, :PENDING_DELIVERY, :DELIVERED)
-	end
-	
-end
